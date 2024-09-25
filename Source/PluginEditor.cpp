818c83e07bcb904170ed5779e5f3f73e1fb252c4
Miroslav Bořek <boremiro@seznam.cz>
2024-09-25 19:52:21 +0200

    save plugin state

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -16,7 +16,7 @@ SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor(
 : AudioProcessorEditor(&p), audioProcessor(p) {
 	// Make sure that before the constructor has finished, you've set the
 	// editor's size to whatever you need it to be.
-	setSize(400, 300);
+	setSize(600, 400);
 }
 
 SimpleEQAudioProcessorEditor::~SimpleEQAudioProcessorEditor() {}
